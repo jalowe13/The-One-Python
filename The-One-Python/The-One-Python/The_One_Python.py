43535feefffee025cc75d4ab9ddad5287d98a1f1
Jacob Lowe <jplowe21@vt.edu>
2020-11-27 16:44:38 -0500

    Account Creation Implementation
    Creating account save file and loading default values into the save file

--- The-One-Python/The-One-Python/The_One_Python.py
+++ The-One-Python/The-One-Python/The_One_Python.py
@@ -1,33 +1,38 @@
 import ctypes
-ctypes.windll.kernel32.SetConsoleTitleA('The One') #Setting the command prompt window opened to a certian title
+
+ctypes.windll.kernel32.SetConsoleTitleA('The One')  # Setting the command prompt window opened to a specific title
+
 
 def login():
     pass
 
-def createAccount():
-    print('create')
+
+def create_account():
+    print('What would you like your username to be?')
+    username = input()
+    print('What would you like your password to be?')
+    password = input()
+
+#   Save File Contents
+    save = open(username + ".py", "w")
+    save.write("username = '" + username + "'\n")
+    save.write("password = '" + password + "'\n")
+    with open("data_values/" + "default_values.py") as default_values:
+        for line in default_values:
+            save.write(line)
+
 
 def title():
-    file = open("art/" + "titleart.txt" , 'r') # Opening the file in a different directory
-    print(file.read())
-    print ('Current Version: v0.3_80 [Jacob Lowe]')
-    print ('1) Login')
-    print ('2) Create Account')
-    print ('3) Exit')
+    title = open("art/" + "titleart.txt", 'r')  # Opening the file in a different directory
+    print(title.read())
+    print('Current Version: v0.3_80 [Jacob Lowe]')
+    print('1) Login')
+    print('2) Create Account')
+    print('3) Exit')
     title_selection = input()
 
-    if (title_selection == '2'):
-        createAccount()
-
-
-
-
-
+    if title_selection == '2':
+        create_account()
 
 
 title()
-
-
-
-
-
