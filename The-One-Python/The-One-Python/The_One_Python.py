5abcf3fad676b7e74cea3c049beacb39534be1cf
Jacob Lowe <jplowe21@vt.edu>
2020-11-28 18:42:37 -0500

    login() implementation and Library
    Need to do login validation next

--- The-One-Python/The-One-Python/The_One_Python.py
+++ The-One-Python/The-One-Python/The_One_Python.py
@@ -1,55 +1,49 @@
 import ctypes
-import json
 
 ctypes.windll.kernel32.SetConsoleTitleA('The One')  # Setting the command prompt window opened to a specific title
 
 
 class Character:
-    name = ''
-    password = ''
-    hp = 0
-    max_hp = 0
-    gold = 0
-    bank_gold = 0
-    flee_gold = 0
-    town = 1
-    level = 1
+    stats = {
+        "username": "",
+        "password": "",
+        "hp": 0,
+        "max_hp": 0,
+        "gold": 0,
+        "bank_gold": 0,
+        "flee_gold": 0,
+        "town": 1,
+        "level": 1
+    }
 
-    def __init__(self, name, password):
-        self.name = name
-        self.password = password
-        self.hp = 100
-        self.max_hp = 100
-        self.gold = 25
-        self.bank_gold = 0
-        self.flee_gold = 50
-        self.town = 1
-        self.level = 1
-        print("Default Values Loaded!")
+    def __init__(self, username, password):
+        self.stats["username"] = username
+        self.stats["password"] = password
+        self.stats["hp"] = 100
+        self.stats["max_hp"] = 100
+        self.stats["gold"] = 25
+        self.stats["bank_gold"] = 0
+        self.stats["flee_gold"] = 50
+        self.stats["town"] = 1
+        self.stats["level"] = 1
+        print("Default Values for new Character " + username + "Loaded!")
 
-    def print_values(self):
-        print("Values for " + self.name)
-        print("Level[" + str(self.level) + "]")
-        print("Password[" + self.password + "]")
-        print("HP[" + str(self.hp) + "/" + str(self.max_hp) + "]")
-        print("Gold[" + str(self.gold) + "]")
-        print("Bank Gold[" + str(self.bank_gold) + "]")
-        print("Flee Gold Cost[" + str(self.flee_gold) + "]")
-        print("Current Town[" + str(self.town) + "]")
+    def print_stats(self):
+        print("Printing Stats for User[" + self.stats["username"] + "]")
+        print(self.stats)
 
 
-def login():
+def login(player):
     print("Username: ")
     username = input()
     print("Password: ")
     password = input()
-    name = ''
-    value = ''
     with open(username + ".txt", "r") as values:
         for line in values:
-            name, value = line.replace(' ', '').strip('=')
-            print('Name: ' + name + " " + "Value: " + value)
-
+            newline = line.split('=')
+            name = newline[0].rstrip()  # Removing newline characters
+            value = newline[1].rstrip()
+            player.stats[name] = value
 
 
 def create_account():
@@ -60,8 +54,8 @@ def create_account():
 
     #   Save File Contents
     save = open(username + ".txt", "w")
-    save.write("username = '" + username + "'\n")
-    save.write("password = '" + password + "'\n")
+    save.write("username=" + username + "\n")
+    save.write("password=" + password + "\n")
     player = Character(username, password)
     with open("data/" + "default_values.txt") as values:
         for line in values:
@@ -80,13 +74,10 @@ def title():
     title_selection = input()
     if title_selection == '2':
         p = create_account()
-        p.print_values()
+        p.print_stats()
     if title_selection == '1':
-        login()
-        p.print_values()
-
+        login(p)
+        p.print_stats()
 
 
 title()
-
-
