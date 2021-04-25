2156065c7695ca05843798a92903303ce80a6b01
jalowe13 <jplowe21@vt.edu>
2021-04-25 18:34:00 -0400

    0.2.0-a
    WASD Input to Console Window title

--- The-One-Gui/WinMain.cpp
+++ The-One-Gui/WinMain.cpp
@@ -14,6 +14,55 @@
 #include <dwrite.h>
 #include <wincodec.h>
 
+//String Handling
+#include <sstream>
+#include <string>
+
+
+//Message Handling
+LRESULT CALLBACK WindowProc(
+    __in HWND hWindow,
+    __in UINT uMsg,
+    __in WPARAM wParam,
+    __in LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_CLOSE:
+        PostQuitMessage(69);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    case WM_KEYDOWN:
+        if (wParam == 'W')
+        {
+            SetWindowText(hWindow, "W");
+        }
+        else if (wParam == 'A')
+        {
+            SetWindowText(hWindow, "A");
+        }
+        else if (wParam == 'S')
+        {
+            SetWindowText(hWindow, "S");
+        }
+        else if (wParam == 'D')
+        {
+            SetWindowText(hWindow, "D");
+        }
+        break;
+    case WM_KEYUP:
+        SetWindowText(hWindow, "The One");
+        break;
+    default:
+        return DefWindowProc(hWindow, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+
 // Main Function Window instance
 int CALLBACK WinMain(
   HINSTANCE hInstance,
@@ -22,11 +71,11 @@ int CALLBACK WinMain(
   int nCmdShow)
   {
 
-    LPCSTR pClassName = "Window Class";
+    LPCSTR pClassName = "newwindow";
     WNDCLASSEX wc = {0};
     wc.cbSize = sizeof( wc );
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WindowProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -46,10 +95,19 @@ int CALLBACK WinMain(
     );
     ShowWindow(hWnd,SW_SHOW);
     MSG msg; //New message instance
-    while (GetMessage( &msg,nullptr,0,0) > 0)
+    BOOL gResult;
+    while ((gResult = GetMessage( &msg,nullptr,0,0)) > 0)
     {
       TranslateMessage( &msg );
       DispatchMessage( &msg );
+    }
+    if (gResult == -1) //Error handling
+    {
+      return -1;
+    }
+    else
+    {
+      return msg.wParam;
     }
     return 0;
   }
